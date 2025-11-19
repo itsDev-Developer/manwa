a724560e5b32c4d6083219cbd315883e24d14b8e
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2025-11-19 12:19:49 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from TG.wks import Bot, worker, asyncio, Vars, remove_expired_users
-from TG.auto import main_updates
-from Tools.my_token import expired_token_
+import uvloop
+import asyncio
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+uvloop.install()
 
+from Tools.auto import main_updates, Vars
+from Tools.my_token import expired_token_
+from Tools.db import remove_expired_users
 import os, shutil
+from bot import Bot 
+from Tools.cworker import worker
 
 folder_path = "Process"
 if os.path.exists(folder_path) and os.path.isdir(folder_path):
@@ -21,11 +27,12 @@ async def main_exp_():
 
 if __name__ == "__main__":
   loop = asyncio.get_event_loop()
-  for i in range(15):
+  for i in range(20):
     loop.create_task(worker(i))
-  
+
   loop.create_task(main_updates())
   if Vars.SHORTENER:
     loop.create_task(main_exp_())
-  
+
   Bot.run()
+  
